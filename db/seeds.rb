bb6823f681d901ecae0106f9ea69c78dd960aff4
Chloe Cosson <cosson.chloe@gmail.com>
2025-03-11 12:10:26 +0100

    commit before pull

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+User.create!(first_name: "John", last_name: "Doe", email: "johndoe@mail.com", password: "password")
