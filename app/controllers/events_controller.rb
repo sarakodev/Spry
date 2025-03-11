9f817b45cfc50eef6165cd37edb2c65519e8398e
bastien duong <bastienduongme@gmail.com>
2025-03-11 16:31:30 +0100

    new

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,5 +5,18 @@ class EventsController < ApplicationController
     def new
         @event = Event.new
     end
-    
+    def create
+        @event = Event.new(event_params)
+        @event.user = current_user
+        if @event.save!
+          redirect_to events_path, notice: "Votre event a bien été créé"
+        else
+          render :new, status: :unprocessable_entity
+        end
+      end
+    private
+
+  def event_params
+    params.require(:event).permit(:name, :pace, :datetime, :description, :participants, :location)
+  end
 end
