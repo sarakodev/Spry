18e781c3a83762a9e667f7a82d490009437159c2
bastien duong <bastienduongme@gmail.com>
2025-03-12 17:16:24 +0100

    card

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,6 +23,6 @@ class EventsController < ApplicationController
     private
 
   def event_params
-    params.require(:event).permit(:name, :pace, :datetime, :description, :participants, :location)
+    params.require(:event).permit(:name, :pace, :datetime, :description, :participants, :location, :kilometers, :photo)
   end
 end
