6540f74ef531053fb5e81d2e7ad4080877dc3bbb
Chloe Cosson <cosson.chloe@gmail.com>
2025-03-13 18:01:03 +0100

    user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,12 @@ class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :events_as_participant, through: :bookings, source: :event
 
-  # has_many :bookings_as_event_organizer, through: :events, source: :bookings
-  # has_many :bookings_as_event_seeker, class_name: "Booking", dependent: :destroy
-
   has_one_attached :photo
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def fullname
+    "#{first_name} #{last_name}"
+  end
 end
