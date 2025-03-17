0b7bd316229ac06df4c8241134efc74a9e96254f
bastien duong <bastienduongme@gmail.com>
2025-03-17 17:01:33 +0100

    navbar

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,7 +7,7 @@ class Message < ApplicationRecord
   private
 
   def broadcast_message
-    broadcast_append_to "chatroom_#{chatrooms.id}_messages",
+    broadcast_append_to "chatroom_#{chatroom.id}_messages",
                         partial: "messages/message",
                         locals: { message: self, user: user },
                         target: "messages"
