326753f70ed826c880e0022b9c9223484ec9715b
Chloe Cosson <cosson.chloe@gmail.com>
2025-03-18 14:54:15 +0100

    generated the new challenge form

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,10 +1,14 @@
 class Challenge < ApplicationRecord
   CATEGORIES = ["Distance challenge", "Duration challenge"]
 
+  has_many :participations
   has_many :users, through: :participations, dependent: :destroy
+  accepts_nested_attributes_for :participations, allow_destroy: true
+  belongs_to :user
 
   validates :title, presence: true
   validates :category, presence: true, inclusion: { in: CATEGORIES }
   validates :team_name, presence: true
+
 
 end
