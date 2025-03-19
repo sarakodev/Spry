044507f81d1d6193d2b62634b8dbadccd6e81866
SarahRakotomanana <s.rako.manana@gmail.com>
2025-03-19 16:21:32 +0100

    small fixes wording and padding challenge show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def format_steps(steps)
+    if steps < 1000
+      "#{steps}"
+    elsif steps < 1_000_000
+      "#{(steps / 1000.0).round(1).to_i == (steps / 1000.0).round(1) ? (steps / 1000.0).round(1).to_i : (steps / 1000.0).round(1)}k"
+    else
+      "#{(steps / 1_000_000.0).round(1).to_i == (steps / 1_000_000.0).round(1) ? (steps / 1_000_000.0).round(1).to_i : (steps / 1_000_000.0).round(1)}M"
+    end
+  end
 end
