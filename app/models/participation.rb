d05f81b0919213129d6f0f53bf3d6af7c3571bba
SarahRakotomanana <s.rako.manana@gmail.com>
2025-03-20 16:26:58 +0100

    new branch

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -4,8 +4,19 @@ class Participation < ApplicationRecord
   after_update :update_challenge_completion
 
   def update_challenge_completion
-    total_distance = challenge.participations.map(&:distance).compact.sum
-    completion = (total_distance.fdiv(challenge.distance))
+    if challenge.distance.present?
+      # Handle distance-based challenges
+      total_distance = challenge.participations.map(&:distance).compact.sum
+      completion = (total_distance.fdiv(challenge.distance))
+    elsif challenge.duration.present?
+      # Handle duration-based challenges
+      total_duration = challenge.participations.map(&:duration).compact.sum
+      completion = (total_duration.fdiv(challenge.duration))
+    else
+      # Handle cases where neither distance nor duration is set
+      completion = 0.0
+    end
+
     challenge.update(completion: completion)
   end
 end
