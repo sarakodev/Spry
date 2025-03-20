7d7f0acd9655a050bb44b67f1d4ffa39fc650c43
Chloe Cosson <cosson.chloe@gmail.com>
2025-03-20 14:47:15 +0100

    margin bottom corrected on events index

--- db/schema.rb
+++ db/schema.rb
@@ -64,11 +64,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_20_114038) do
     t.string "start_point"
     t.string "end_point"
     t.string "team_name"
+    t.bigint "user_id", null: false
     t.float "start_latitude"
     t.float "start_longitude"
     t.float "end_latitude"
     t.float "end_longitude"
-    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_challenges_on_user_id"
   end
 
